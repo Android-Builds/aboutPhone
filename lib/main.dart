2f05daf828853fb6e60d351424c95091e38dd012
Debjit Chakraborty <zxyron0@gmail.com>
2020-07-04 21:48:09 +0530

    Add custom cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,177 @@
+import 'dart:async';
+
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:device_info/device_info.dart';
 
 void main() {
-  runApp(MyApp());
+  runZoned(() {
+    runApp(MyApp());
+  }, onError: (dynamic error, dynamic stack) {
+    print(error);
+    print(stack);
+  });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    Map<String, dynamic> deviceData;
+
+    try {
+      deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _deviceData = deviceData;
+    });
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'androidId': build.androidId,
+      'systemFeatures': build.systemFeatures,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: [
+              ListTile(
+                title: InfoCard(
+                  height: 200.0,
+                  widget: Text('Hi'),
+                ),
+              ),
+              ListTile(
+                title: InfoCard(
+                  height: 150.0,
+                  widget: Text('Hi'),
+                ),
+              ),
+              ListTile(
+                title: InfoCard(
+                  height: 100.0,
+                  widget: Text(
+                    _deviceData['version.baseOS'],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class InfoCard extends StatelessWidget {
+  const InfoCard({
+    Key key,
+    this.height,
+    this.widget,
+  }) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final height;
+  final Widget widget;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Flexible(
+      child: SizedBox(
+        height: height,
+        width: 300.0,
+        child: Card(
+          color: Colors.grey[200],
+          margin: EdgeInsets.all(5.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Center(
+            child: widget,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
 }
+
+// ListView(
+//           children: _deviceData.keys.map(
+//             (String property) {
+//               return Row(
+//                 children: <Widget>[
+//                   Container(
+//                     padding: const EdgeInsets.all(10.0),
+//                     child: Text(
+//                       property,
+//                       style: const TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                       ),
+//                     ),
+//                   ),
+//                   Expanded(
+//                     child: Container(
+//                       padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+//                       child: Text(
+//                         '${_deviceData[property]}',
+//                         maxLines: 10,
+//                         overflow: TextOverflow.ellipsis,
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               );
+//             },
+//           ).toList(),
+//         ),
